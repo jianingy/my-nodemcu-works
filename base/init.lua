738033bbd74cabcb379376242b65e3e7bbdef376
Jianing Yang <jianingy.yang@gmail.com>
2016-08-03 14:03:15 +0800

    message update

--- base/init.lua
+++ base/init.lua
@@ -19,7 +19,7 @@ do
    local pin, console = {led = settings.pin.led, tact = 3}, false
 
    -- disable serial/uart (console)
-   print('starting jianingy nodemcu system. version: 0.1.2')
+   print('starting nodemcu base system. version: 0.1.2')
    print('Press ENTER or KEY_FLASH to enable the console.')
    uart.on('data', '\r',
            function(data)
